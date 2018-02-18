c17d45e1cce9abb80b3003dbb1aaafdea63541ff
Jason Walzak <walzak@gmail.com>
2018-02-17 19:11:01 -0500

    updated comments on ajax.php

--- ajax.php
+++ ajax.php
@@ -10,7 +10,7 @@ $playerSummarySteam = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries
 $playedGames = 'http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key='.$ACCESS_KEY.'&steamid='.$STEAM_ID.'&format=json';
 $gamesList = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?include_appinfo=1&include_played_free_games=1&key='.$ACCESS_KEY.'&steamid='.$STEAM_ID.'&format=json';
 
-
+// To know what function to run when JavaScript calls for JSON data
 if(isset($_GET['action'])) {
   header('Content-Type: application/json');
   if($_GET['action'] == 'steam'){
@@ -25,14 +25,11 @@ if(isset($_GET['action'])) {
 }
 
 function getSteamData($url){
-  $curl = curl_init();
-  // $file = 'data.json';
-  
+  $curl = curl_init();  
   curl_setopt_array($curl, array(
     CURLOPT_RETURNTRANSFER => 1,
     CURLOPT_URL => $url
   ));
-  
   $resp = curl_exec($curl);
   curl_close($curl);
   
